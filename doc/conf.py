c4087435ccfc82a026d929d0c857178498805ec5
ZaPpInG <lrmoreno007@gmail.com>
2018-02-20 15:25:49 +0100

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -19,7 +19,16 @@
 import os
 import sys
 # sys.path.insert(0, os.path.abspath('.'))
+import recommonmark
+from recommonmark.transform import AutoStructify
 
+# At the bottom of conf.py
+def setup(app):
+    app.add_config_value('recommonmark_config', {
+            'url_resolver': lambda url: github_doc_root + url,
+            'auto_toc_tree_section': 'Contents',
+            }, True)
+    app.add_transform(AutoStructify)
 
 # -- General configuration ------------------------------------------------
 
