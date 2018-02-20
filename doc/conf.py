6e9d85ac732c1fdd6e9c23c0d010657ae9103558
ZaPpInG <lrmoreno007@gmail.com>
2018-02-20 15:28:17 +0100

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -22,13 +22,7 @@ import sys
 import recommonmark
 from recommonmark.transform import AutoStructify
 
-# At the bottom of conf.py
-def setup(app):
-    app.add_config_value('recommonmark_config', {
-            'url_resolver': lambda url: github_doc_root + url,
-            'auto_toc_tree_section': 'Contents',
-            }, True)
-    app.add_transform(AutoStructify)
+
 
 # -- General configuration ------------------------------------------------
 
@@ -175,4 +169,10 @@ if not env_readthedocs:  # only import and set the theme if we're building docs 
     html_theme = 'sphinx_rtd_theme'
     html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
-
+# At the bottom of conf.py
+def setup(app):
+    app.add_config_value('recommonmark_config', {
+            'url_resolver': lambda url: github_doc_root + url,
+            'auto_toc_tree_section': 'Contents',
+            }, True)
+    app.add_transform(AutoStructify)
