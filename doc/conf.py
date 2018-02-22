60ed7254157d97c1714ae033a1619541233d08dd
ZaPpInG <lrmoreno007@gmail.com>
2018-02-22 14:12:58 +0100

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -46,11 +46,7 @@ templates_path = ['_templates']
 # You can specify multiple suffix as a list of string:
 #
 # source_suffix = ['.rst', '.md']
-source_suffix = [
-    '.rst': 'restructuredtext',
-    '.txt': 'restructuredtext',
-    '.md': 'markdown',
-]
+source_suffix = '.rst'
 
 # The master toctree document.
 master_doc = 'index'
