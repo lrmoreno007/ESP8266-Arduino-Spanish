991c1d21abca94d3b3594595c4067f052ed19ee2
ZaPpInG <lrmoreno007@gmail.com>
2018-03-07 21:42:12 +0100

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -312,7 +312,7 @@ env.docname, link_id)
 def setup(app):
     app.add_transform(CreateSectionLabels)  
     """Initialize Sphinx extension."""
-"""    _add_notebook_parser(app)
+    _add_notebook_parser(app)
 
     app.add_config_value('nbsphinx_execute', 'auto', rebuild='env')
     app.add_config_value('nbsphinx_kernel_name', '', rebuild='env')
@@ -325,7 +325,7 @@ def setup(app):
     app.add_config_value('nbsphinx_responsive_width', '540px', rebuild='html')
     app.add_config_value('nbsphinx_prolog', None, rebuild='env')
     app.add_config_value('nbsphinx_epilog', None, rebuild='env')
-
+"""
     app.add_directive('nbinput', NbInput)
     app.add_directive('nboutput', NbOutput)
     app.add_directive('nbinfo', NbInfo)
