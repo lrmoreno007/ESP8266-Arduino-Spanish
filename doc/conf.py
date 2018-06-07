1e5715babb61a6388966a421912b3bfae9bd2984
ZaPpInG <lrmoreno007@gmail.com>
2018-06-07 19:25:12 +0200

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -30,7 +30,9 @@ import sys
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [nbsphinx]
+extensions = [
+    'nbsphinx'
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
